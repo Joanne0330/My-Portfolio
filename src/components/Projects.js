de0235110121edc01f8737f420f94013bf4dfa4e
Joanne Chen <joanne_r_chen@yahoo.com>
2020-10-27 15:41:56 +0000

    data now in a class with state and can parse the title

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,24 +1,32 @@
+import { render } from "@testing-library/react";
 import React from "react";
 
-function Projects(props) {
-  const data = [
-      {
-          title: "AIsha - Artificial Intelligence Self Help App",
-          description: "A machine learning chatbot which analyses, responds to, and records users' emotions (joy, sadness, anger and fear)",
-          tech: "React, Flask, Python",
-          link: "https://github.com/aravzpatel/AIsha",
-          key: 1
-      },
-  ]
+class Projects extends React.Component {
+    constructor(props) {
+    super(props);
+    this.state = {
+        data:[
+            {
+            title: "AIsha - Artificial Intelligence Self Help App",
+            description: "A machine learning chatbot which analyses, responds to, and records users' emotions (joy, sadness, anger and fear)",
+            tech: "React, Flask, Python",
+            link: "https://github.com/aravzpatel/AIsha",
+            key: 1
+        }
+     ]
+    }
+}
 
-
-
-    return (
-        <div>
-          <h2>where's the data?</h2>  
-
-         </div>
-    )  
+    render() {
+        return (
+            <div>
+              <h2>where's the data?</h2>  
+              {this.state.data.map((project, index) => {
+                return <p>{project.title}</p>
+              })}
+            </div>
+        )
+    }
 }
 
 export default Projects;

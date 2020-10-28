ee120ae1206a04ec1508670ff211112671ec2d92
Joanne Chen <joanne_r_chen@yahoo.com>
2020-10-28 13:34:06 +0000

    imported images for projects and styling finished

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,6 +1,14 @@
 // import { render } from "@testing-library/react";
 import React from "react";
+import '../App.css';
 import Card from "react-bootstrap/Card";
+import AIsha from "../images/AIsha.png";
+import Jem from "../images/Jem.png";
+import Tetris from "../images/Tetris.png";
+import Dreambook from "../images/Dreambook.png";
+import ShoppingList from "../images/ShoppingList.png";
+import BankTechTest from "../images/BankTechTest.png";
+import Takeaway from "../images/Takeaway.png";
 
 class Projects extends React.Component {
     constructor(props) {
@@ -9,13 +17,15 @@ class Projects extends React.Component {
         data:[
             {
                 title: "AIsha - Artificial Intelligence Self Help App",
-                description: "A machine learning chatbot which analyses, responds to, and records users' daily emotions (joy, sadness, anger and fear). My final project at Makers Academy",
+                src: AIsha,
+                description: "A machine learning chatbot which analyses, responds to, and tracks users' daily emotions (joy, sadness, anger and fear) in a form of graph. This was my final project at Makers Academy",
                 tech: "React, Flask, Python",
                 link: "https://github.com/aravzpatel/AIsha",
                 key: 1
             },
             {
                 title: "Jem Photo Share",
+                src: Jem,
                 description: "A hybird of Instagram and Facebook social media",
                 tech: "Ruby, Rails, MongoDB",
                 link: "https://github.com/Joanne0330/Jem-Photoshare",
@@ -23,13 +33,15 @@ class Projects extends React.Component {
             },
             {
                 title: "Tetris Interactive Game",
-                description: "An interactive, traditional tetris game made by pure Vanilla JavaScript",
+                src: Tetris,
+                description: "An interactive, traditional tetris game built with pure Vanilla JavaScript",
                 tech: "JavaScript",
                 link: "https://github.com/Joanne0330/Tetris-interactive-game",
                 key: 3
             },
             {
                 title: "Dreambook",
+                src: Dreambook,
                 description: "A mock Facebook social media. Group project at Makers Academy",
                 tech: "Ruby, Rails, PostgresSQL",
                 link: "https://github.com/katieljones/acebook-dreambook-2020",
@@ -37,37 +49,37 @@ class Projects extends React.Component {
             },
             {
                 title: "Shopping List",
+                src: ShoppingList,
                 description: "Front-end app, perfect to use as shopping or todo list",
                 tech: "React",
                 link: "https://github.com/Joanne0330/shopping-list-react",
                 key: 5
             },
             {
+                title: "Bank Tech Test",
+                src: BankTechTest,
+                description: "A demonstration of clean code, unit and feature testing, written in Ruby. User can make desposti and withraw of a bank account, and print receipts and statements ",
+                tech: "Ruby",
+                link: "https://github.com/Joanne0330/BankTechTest",
+                key: 6
+            },
+            {
+                title: "Takeaway Challenge",
+                src: Takeaway,
+                description: "A demonstration of clean code, unit tests, written in JavaScript. User can order meals from a menu and have price calculated",
+                tech: "JavaScript",
+                link: "https://github.com/Joanne0330/Takeaway-JavaScript",
+                key: 7
+            },
+            {
                 title: "My Portfolio",
                 description: "This website you're looking at!",
                 tech: "React",
                 link: "https://github.com/Joanne0330/My-Portfolio",
-                key: 6
-            },
-            {
-                title: "Bank Tech Test",
-                description: "A demonstration of clean code, unit and feature testing, written in Ruby. User can make desposti and withraw of a bank account, and print receipts and statements ",
-                tech: "Ruby",
-                link: "https://github.com/Joanne0330/BankTechTest",
-                key: 7
-            },
-            {
-                title: "Takeaway Challenge",
-                description: "A demonstration of clean code, unit tests, written in JavaScript. User can order meals from a menu and have price calculated",
-                tech: "JavaScript",
-                link: "https://github.com/Joanne0330/Takeaway-JavaScript",
                 key: 8
-            }
-            
-
-
-     ]
-    }
+            },   
+            ]
+        }
 }
 
     render() {
@@ -78,12 +90,18 @@ class Projects extends React.Component {
                 return (
 
                 <Card key={project.key} style={cardStyle}>
+                    <Card.Img src={project.src} />
                     <Card.Body>
                         <Card.Title>{project.title}</Card.Title>
                         <Card.Text>{project.description}</Card.Text>
-                        <Card.Link href={project.link}>See the code</Card.Link>
+                        {/* <Card.Link href={project.link} target="_blank">See the code</Card.Link> */}
                     </Card.Body>
-                    <Card.Footer className="text-muted">{project.tech}</Card.Footer>    
+                    <Card.Footer className="text-muted" style={{margin: '0.2rem'}}>
+                        {project.tech}
+                    </Card.Footer>    
+                    <Card.Footer style={{margin: '0.2rem'}}>
+                        <Card.Link href={project.link} target="_blank">See the code</Card.Link>
+                    </Card.Footer>
                 </Card>
 
                 )    
@@ -98,15 +116,17 @@ export default Projects;
 const cardStyle = {
     minWidth: '20rem',
     maxWidth: '20rem',
-    minHeight: '35rem',
-    maxHeight: '35rem',
+    minHeight: '40rem',
+    maxHeight: '40rem',
     padding: '2rem',
     margin: '2rem',
-    boxShadow: '5px 10px 8px #a1a0a0',
+    // boxShadow: '15px 17px 12px #a1a0a0',
+    boxShadow: '18px 18px 18px #778899'
 }
 
 const pageStyle = {
     display: 'flex', 
     flexDirection: 'row', 
-    flexWrap: 'wrap'
+    flexWrap: 'wrap',
+    justifyContent: 'space-evenly'
 }

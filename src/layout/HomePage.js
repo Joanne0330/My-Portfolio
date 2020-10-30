f3e2b4411f4d7a6f1b0b37608ca1dba419c9593f
Joanne Chen <joanne_r_chen@yahoo.com>
2020-10-30 15:57:30 +0000

    deploying on heroku

--- src/layout/HomePage.js
+++ src/layout/HomePage.js
@@ -6,11 +6,9 @@ export default function ProjectsPage() {
         <div>
                 <header style={HomePageStyle}><h1>Joanne Chen</h1></header>
                 <Home />
-
         </div>
     )
 }
-
 const HomePageStyle = {
     margin: '2rem', 
     backgroundColor: 'darkgray', 

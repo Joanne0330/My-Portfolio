77f544d0d7e9e71885633876653cb645b482fa5a
Joanne Chen <joanne_r_chen@yahoo.com>
2020-11-02 11:31:34 +0000

    deploy failed, return comment out deploy in package.json

--- src/index.js
+++ src/index.js
@@ -3,14 +3,14 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { BrowserRouter as Router } from "react-router-dom";
+// import { BrowserRouter as Router } from "react-router-dom";
 
 ReactDOM.render(
   <React.StrictMode>
-    <Router basename={process.env.PUBLIC_URL}>
+    {/* <Router basename={process.env.PUBLIC_URL}> */}
       <App />
 
-    </Router>
+    {/* </Router> */}
   </React.StrictMode>,
   document.getElementById('root')
 );
